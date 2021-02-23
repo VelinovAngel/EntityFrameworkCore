5bb9a1760736cba1057d0d9db2215689401439cb
VelinovAngl <kakarum1992@gmail.com>
2021-02-23 20:26:57 +0100

    Craate method GetEmployeesFromResearchAndDevelopment

--- ENTITY FRAMEWORK INTRODUCTION/EntityFrameworkCore/SoftUni/StartUp.cs
+++ ENTITY FRAMEWORK INTRODUCTION/EntityFrameworkCore/SoftUni/StartUp.cs
@@ -12,10 +12,35 @@ namespace SoftUni
             var context = new SoftUniContext();
 
             //Console.WriteLine(GetEmployeesFullInformation(context));
-            Console.WriteLine(GetEmployeesWithSalaryOver50000(context));
+            //Console.WriteLine(GetEmployeesWithSalaryOver50000(context));
+            Console.WriteLine(GetEmployeesFromResearchAndDevelopment(context));
+
         }
 
+        public static string GetEmployeesFromResearchAndDevelopment(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
 
+            var employees = context.Employees
+                .Where(x => x.Department.Name == "Research and Development")
+                .Select(x => new
+                {
+                    x.FirstName,
+                    x.LastName,
+                    x.Salary,
+                    x.Department.Name
+                })
+                .OrderBy(x => x.Salary)
+                .ThenByDescending(x => x.FirstName)
+                .ToList();
+
+            foreach (var employee in employees)
+            {
+                sb.AppendLine($"{employee.FirstName} {employee.LastName} from {employee.Name} - ${employee.Salary:f2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
         public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
         {
             StringBuilder sb = new StringBuilder();
@@ -49,15 +74,15 @@ namespace SoftUni
                     x.JobTitle,
                     x.Salary
                 })
-                .OrderBy(x=>x.EmployeeId)
+                .OrderBy(x => x.EmployeeId)
                 .ToList();
 
             StringBuilder sb = new StringBuilder();
 
             foreach (var emp in employees)
             {
-               sb.AppendLine($"{emp.FirstName} {emp.LastName} {emp.MiddleName} {emp.JobTitle} {emp.Salary:f2}");
-               
+                sb.AppendLine($"{emp.FirstName} {emp.LastName} {emp.MiddleName} {emp.JobTitle} {emp.Salary:f2}");
+
             }
 
             return sb.ToString().TrimEnd();

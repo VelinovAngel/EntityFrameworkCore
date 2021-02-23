c2f9904335a6ec5591a047ee55cad28f42ea1918
VelinovAngl <kakarum1992@gmail.com>
2021-02-23 19:24:05 +0100

    Craate method GetEmployeesFullInformation

--- ENTITY FRAMEWORK INTRODUCTION/EntityFrameworkCore/SoftUni/StartUp.cs
+++ ENTITY FRAMEWORK INTRODUCTION/EntityFrameworkCore/SoftUni/StartUp.cs
@@ -1,13 +1,45 @@
 ﻿using System;
+using System.Linq;
+using System.Text;
+using SoftUni.Data;
 
 namespace SoftUni
 {
-    class StartUp
+    public class StartUp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var context = new SoftUniContext();
 
+            Console.WriteLine(GetEmployeesFullInformation(context));
+        }
+
+
+
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            var employees = context.Employees
+                .Select(x => new
+                {
+                    x.EmployeeId,
+                    x.FirstName,
+                    x.LastName,
+                    x.MiddleName,
+                    x.JobTitle,
+                    x.Salary
+                })
+                .OrderBy(x=>x.EmployeeId)
+                .ToList();
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var emp in employees)
+            {
+               sb.AppendLine($"{emp.FirstName} {emp.LastName} {emp.MiddleName} {emp.JobTitle} {emp.Salary:f2}");
+               
+            }
+
+            return sb.ToString().TrimEnd();
         }
     }
 }

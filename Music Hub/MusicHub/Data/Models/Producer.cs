0258f538d6bcaaaa8564c58a5f9944056f920c05
VelinovAngl <kakarum1992@gmail.com>
2021-03-05 22:21:52 +0100

    new project

---
+++ Music Hub/MusicHub/Data/Models/Producer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace MusicHub.Data.Models
+{
+    public class Producer
+    {
+        public int Id { get; set; }
+
+        [MaxLength(30)]
+        public string Name { get; set; }
+
+        public string Pseudonym { get; set; }
+
+        public string PhoneNumber { get; set; }
+        public ICollection<Album> Albums { get; set; }
+
+    }
+    //•	Id – Integer, Primary Key
+    //•	Name – text with max length 30 (required)
+    //•	Pseudonym – text
+    //•	PhoneNumber – text
+    //•	Albums – collection of type Album
+
+}
---
+++ Music Hub/MusicHub/Data/Models/SongPerformer.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MusicHub.Data.Models
+{
+    public class SongPerformer
+    {
+        public int SongId { get; set; }
+
+        [Required]
+        public Song Song { get; set; }
+
+        [Required]
+        public int PerformerId { get; set; }
+
+        public Performer Performer { get; set; }
+    }
+    //•	SongId – Integer, Primary Key
+    //•	Song – the performer’s Song(required)
+    //•	PerformerId – Integer, Primary Key
+    //•	Performer – the song’s Performer(required)
+
+}

4e5ba3ee9e8d8df49c609d1456ebeed351b8ea76
VelinovAngl <kakarum1992@gmail.com>
2021-03-07 22:49:59 +0100

    add new problem

---
+++ AdvancedQuerying/BookShop/StartUp.cs
@@ -0,0 +1,30 @@
+﻿namespace BookShop
+{
+    using Data;
+    using Initializer;
+    using System;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var context = new BookShopContext();
+            DbInitializer.ResetDatabase(context);
+
+            string command = Console.ReadLine().ToLower();
+
+            Console.WriteLine(GetBooksByAgeRestriction(context, command));
+        }
+
+        public static string GetBooksByAgeRestriction(BookShopContext context, string command)
+        {
+            StringBuilder sb = new StringBuilder();
+
+
+
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}

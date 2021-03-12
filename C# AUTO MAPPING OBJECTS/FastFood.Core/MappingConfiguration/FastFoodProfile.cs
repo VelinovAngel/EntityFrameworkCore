e264f78aebbd234c0d4ce89503cb07e327edd7bb
VelinovAngl <kakarum1992@gmail.com>
2021-03-12 22:21:26 +0100

    Create item controller

--- C# AUTO MAPPING OBJECTS/FastFood.Core/MappingConfiguration/FastFoodProfile.cs
+++ C# AUTO MAPPING OBJECTS/FastFood.Core/MappingConfiguration/FastFoodProfile.cs
@@ -4,6 +4,7 @@
 
     using FastFood.Models;
     using ViewModels.Positions;
+    using FastFood.Core.ViewModels.Items;
     using FastFood.Core.ViewModels.Employees;
     using FastFood.Core.ViewModels.Categories;
 
@@ -31,9 +32,17 @@
 
 
             //Category
-            this.CreateMap<CreateCategoryInputModel, Category>();
-
             this.CreateMap<Category, CategoryAllViewModel>();
+
+            //Item
+            this.CreateMap<Category, CreateItemViewModel>()
+                .ForMember(x => x.CategoryId, y => y.MapFrom(s => s.Id));
+
+            this.CreateMap<Item, ItemsAllViewModels>()
+            .ForMember(x => x.Category, y => y.MapFrom(s => s.Category.Name));
+
+            this.CreateMap<CreateItemInputModel, Item>();
+
         }
     }
 }

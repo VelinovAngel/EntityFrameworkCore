e9b7bff60aa4141408934ed1d32d8d77fd7e9c39
VelinovAngl <kakarum1992@gmail.com>
2021-03-15 19:42:13 +0100

     .OrderBy(x => x.lastName)

--- JSON PROCESSING/Product Shop/ProductShop/StartUp.cs
+++ JSON PROCESSING/Product Shop/ProductShop/StartUp.cs
@@ -22,16 +22,62 @@ namespace ProductShop
             //string inputJsonUser = File.ReadAllText("../../../Datasets/users.json");
             //string inputJsonProduct = File.ReadAllText("../../../Datasets/products.json");
             //string inputJsonCategories = File.ReadAllText("../../../Datasets/categories.json");
-            string inputJsonCategories = File.ReadAllText("../../../Datasets/categories-products.json");
+            //string inputJsonCategories = File.ReadAllText("../../../Datasets/categories-products.json");
 
             //var resultUsers = ImportUsers(context, inputJsonUser);
             //var resultProduct = ImportProducts(context, inputJsonProduct);
             //var resultCategories = ImportCategories(context, inputJsonCategories);
-            var resultCategoriesProducts = ImportCategoryProducts(context, inputJsonCategories);
+            //var resultCategoriesProducts = ImportCategoryProducts(context, inputJsonCategories);
 
 
-            Console.WriteLine(resultCategoriesProducts);
+            //Console.WriteLine(GetSoldProducts(context));
+            File.WriteAllText("../../../users-sold-products.json", GetSoldProducts(context));
         }
+        public static string GetSoldProducts(ProductShopContext context)
+        {
+            var users = context.Users
+                .Where(x => x.ProductsSold.Any(b => b.Buyer != null))
+                .Where(x => x.ProductsSold.Count > 0)
+                .OrderBy(x => x.LastName)
+                .ThenBy(x => x.FirstName)
+                .Select(x => new
+                {
+                    firstName = x.FirstName,
+                    lastName = x.LastName,
+                    soldProducts = x.ProductsSold.Select(p => new
+                    {
+                        name = p.Name,
+                        price = p.Price,
+                        buyerFirstName = p.Buyer.FirstName,
+                        buyerLastName = p.Buyer.LastName
+                    }
+                    )
+                })
+                .ToList();
+
+            var usersToJson = JsonConvert.SerializeObject(users, Formatting.Indented);
+
+            return usersToJson;
+        }
+
+        public static string GetProductsInRange(ProductShopContext context)
+        {
+            var products = context.Products
+                .Where(x => x.Price >= 500 && x.Price <= 1000)
+                .Select(x => new
+                {
+                    name = x.Name,
+                    price = x.Price,
+                    seller = x.Seller.FirstName + " " + x.Seller.LastName
+                })
+                .OrderBy(x => x.price)
+                .ToList();
+
+            var productsToJson = JsonConvert.SerializeObject(products, Formatting.Indented);
+
+            return productsToJson;
+        }
+
         public static string ImportCategoryProducts(ProductShopContext context, string inputJson)
         {
             var dtoCategoriesProducts = JsonConvert.DeserializeObject<IEnumerable<CategoriesAndProductsInputModel>>(inputJson);
@@ -46,28 +92,29 @@ namespace ProductShop
 
             return $"Successfully imported {categoriesProducts.Count()}";
         }
+
         public static string ImportCategories(ProductShopContext context, string inputJson)
         {
             //Import the users from the provided file categories.json.Some of the names will be null, so you don’t have to add them in the database. Just skip the record and continue.
-            var dtoCategories = JsonConvert.DeserializeObject<IEnumerable<CategoryInputModel>>(inputJson);
+            var dtoCategories = JsonConvert.DeserializeObject<IEnumerable<CategoryInputModel>>(inputJson, new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore });
 
             InitializeAutoMapper();
 
             var categories = mapper.Map<IEnumerable<Category>>(dtoCategories);
 
-            int count = 0;
-            foreach (var category in categories)
-            {
-                if (category.Name != null)
-                {
-                    context.Add(category);
-                    count++;
-                }
-            }
+            //int count = 0;
+            //foreach (var category in categories)
+            //{
+            //    if (category.Name != null)
+            //    {
+            //        context.Add(category);
+            //        count++;
+            //    }
+            //}
 
             context.SaveChanges();
 
-            return $"Successfully imported {count}";
+            return $"Successfully imported {dtoCategories.Count()}";
         }
 
         public static string ImportProducts(ProductShopContext context, string inputJson)

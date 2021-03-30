577f478ae53a8861f0b814a35f6ea18f5f6dcc33
VelinovAngl <kakarum1992@gmail.com>
2021-03-30 22:12:27 +0200

    Export Prisoners by cell id

--- Exam Preparation/C# DB Advanced Retake Exam - 14 August 2020/SoftJail/SoftJail/DataProcessor/Serializer.cs
+++ Exam Preparation/C# DB Advanced Retake Exam - 14 August 2020/SoftJail/SoftJail/DataProcessor/Serializer.cs
@@ -2,13 +2,39 @@
 {
 
     using Data;
+    using Newtonsoft.Json;
     using System;
+    using System.IO;
+    using System.Linq;
 
     public class Serializer
     {
         public static string ExportPrisonersByCells(SoftJailDbContext context, int[] ids)
         {
-            return "TODO";
+            var prisons = context.Prisoners
+                .ToList()
+                .Where(x => ids.Contains(x.Id))
+                .Select(x => new
+                {
+                    Id = x.Id,
+                    Name = x.FullName,
+                    CellNumber = x.Cell.CellNumber,
+                    Officers = x.PrisonerOfficers.Select(o => new
+                    {
+                        OfficerName = o.Officer.FullName,
+                        Department = o.Officer.Department.Name
+                    })
+                    .OrderBy(n => n.OfficerName)
+                    .ToList(),
+                    TotalOfficerSalary = decimal.Parse(x.PrisonerOfficers.Sum(s => s.Officer.Salary).ToString("f2"))
+                })
+                .OrderBy(x => x.Name)
+                .ThenBy(x => x.Id)
+                .ToList();
+
+           var prisonsToJson =  JsonConvert.SerializeObject(prisons, Formatting.Indented);
+
+            return prisonsToJson;
         }
 
         public static string ExportPrisonersInbox(SoftJailDbContext context, string prisonersNames)

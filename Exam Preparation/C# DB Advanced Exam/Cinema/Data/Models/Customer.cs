ee5ab92b4f87dfd5a1ec6e79d6943da771f2645f
VelinovAngl <kakarum1992@gmail.com>
2021-04-02 22:25:44 +0200

    add entity Customer

---
+++ Exam Preparation/C# DB Advanced Exam/Cinema/Data/Models/Customer.cs
@@ -0,0 +1,40 @@
+﻿namespace Cinema.Data.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+
+    public class Customer
+    {
+        public Customer()
+        {
+            this.Tickets = new HashSet<Ticket>();
+        }
+        public int Id { get; set; }
+        //• Id – integer, Primary Key
+
+        [Required]
+        [MinLength(3), MaxLength(20)]
+        public string FirstName { get; set; }
+        //• FirstName – text with length[3, 20] (required)
+
+        [Required]
+        [MinLength(3), MaxLength(20)]
+        public string LastName { get; set; }
+        //• LastName – text with length[3, 20] (required)
+
+        [Required]
+        [Range(12, 110)]
+        public int Age { get; set; }
+        //• Age – integer in the range[12, 110] (required)
+
+        [Required]
+        [Range(0.01, double.MaxValue)]
+        public decimal Balance { get; set; }
+        //• Balance - decimal (non-negative, minimum value: 0.01) (required)
+
+        public ICollection<Ticket> Tickets { get; set; }
+        //• Tickets - collection of type Ticket
+    }
+}

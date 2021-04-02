b95db8b42e8f2b6d755d5a2b1b4747de9bad1f0d
VelinovAngl <kakarum1992@gmail.com>
2021-04-02 23:14:19 +0200

    add import method for movies

--- Exam Preparation/C# DB Advanced Exam/Cinema/DataProcessor/Deserializer.cs
+++ Exam Preparation/C# DB Advanced Exam/Cinema/DataProcessor/Deserializer.cs
@@ -1,9 +1,15 @@
 ﻿namespace Cinema.DataProcessor
 {
+    using Cinema.Data.Models;
+    using Cinema.Data.Models.Enums;
+    using Cinema.DataProcessor.ImportDto;
     using Data;
+    using Newtonsoft.Json;
+    using System;
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
-
+    using System.Linq;
+    using System.Text;
 
     public class Deserializer
     {
@@ -19,8 +25,43 @@
 
         public static string ImportMovies(CinemaContext context, string jsonString)
         {
+            var result = new StringBuilder();
 
-            return "TODO";
+            var moviesJson = JsonConvert.DeserializeObject<IEnumerable<ImportMoviesModels>>(jsonString);
+
+            foreach (var movie in moviesJson)
+            {
+                if (!IsValid(movie))
+                {
+                    result.AppendLine(ErrorMessage);
+                    continue;
+                }
+
+                var existTitile = context.Movies.FirstOrDefault(x => x.Title == movie.Title);
+
+                if (existTitile != null)
+                {
+                    result.AppendLine(ErrorMessage);
+                    continue;
+                }
+
+                var currGenre = Enum.Parse<Genre>(movie.Genre);
+
+                var currMovie = new Movie
+                {
+                    Title = movie.Title,
+                    Genre = currGenre,
+                    Duration = TimeSpan.Parse(movie.Duration),
+                    Rating = movie.Rating,
+                    Director = movie.Director
+                };
+
+                context.Movies.Add(currMovie);
+                context.SaveChanges();
+                result.AppendLine(string.Format(SuccessfulImportMovie, currMovie.Title, currMovie.Genre, currMovie.Rating));
+            }
+
+            return result.ToString().TrimEnd();
         }
 
         public static string ImportHallSeats(CinemaContext context, string jsonString)

cadc12b4e0a9ebb34c611b553fb5446515338238
VelinovAngl <kakarum1992@gmail.com>
2021-04-02 22:28:34 +0200

    add entity Ticket

---
+++ Exam Preparation/C# DB Advanced Exam/Cinema/Data/Models/Ticket.cs
@@ -0,0 +1,33 @@
+﻿namespace Cinema.Data.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Text;
+
+
+    public class Ticket
+    {
+        public int Id { get; set; }
+        //Id – integer, Primary Key
+
+        [Required]
+        [Range(0.01, double.MaxValue)]
+        public decimal Price { get; set; }
+        //• Price – decimal (non-negative, minimum value: 0.01) (required)
+
+        [Required]
+        public int CustomerId { get; set; }
+        //• CustomerId – integer, foreign key(required)
+
+        public Customer Customer { get; set; }
+        //• Customer – the customer’s ticket
+
+        [Required]
+        public int ProjectionId { get; set; }
+        //• ProjectionId – integer, foreign key(required)
+
+        public Projection Projection { get; set; }
+        //• Projection – the projection’s ticket
+    }
+}

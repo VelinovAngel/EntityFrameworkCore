f91be420b828027bb561893f157a1f5b4ea2a4d6
VelinovAngl <kakarum1992@gmail.com>
2021-04-02 22:38:18 +0200

    add entity Ticket

--- Exam Preparation/C# DB Advanced Exam/Cinema/Data/CinemaContext.cs
+++ Exam Preparation/C# DB Advanced Exam/Cinema/Data/CinemaContext.cs
@@ -10,6 +10,13 @@
         public CinemaContext(DbContextOptions options)
             : base(options) { }
 
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Hall> Halls { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Projection> Projections { get; set; }
+        public DbSet<Seat> Seats { get; set; }
+        public DbSet<Ticket> Tickets { get; set; }
+
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
---
+++ Exam Preparation/C# DB Advanced Exam/Cinema/Data/Models/Seat.cs
@@ -0,0 +1,17 @@
+﻿namespace Cinema.Data.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class Seat
+    {
+        public int Id { get; set; }
+        //• Id – integer, Primary Key
+
+        [Required]
+        public int HallId { get; set; }
+        //• HallId – integer, foreign key(required)
+
+        public Hall Hall { get; set; }
+        //• Hall – the seat’s hall
+    }
+}

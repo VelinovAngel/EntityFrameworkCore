6c1ff6aa3ab7fc15d0eaf16141198280c13c0595
VelinovAngl <kakarum1992@gmail.com>
2021-04-02 22:14:04 +0200

    add entity Movie

--- Exam Preparation/C# DB Advanced Exam/Cinema/Data/Models/Movie.cs
+++ Exam Preparation/C# DB Advanced Exam/Cinema/Data/Models/Movie.cs
@@ -1,16 +1,45 @@
 ﻿namespace Cinema.Data.Models
 {
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+
+    using Cinema.Data.Models.Enums;
+
     class Movie
     {
-
+        public Movie()
+        {
+            this.Projections = new HashSet<Projection>();
+        }
+        public int Id { get; set; }
         // Id – integer, Primary Key
+
+        [Required]
+        [MinLength(3), MaxLength(20)]
+        public string Title { get; set; }
         //• Title – text with length[3, 20] (required)
+
+        [Required]
+        public Genre Genre { get; set; }
         //• Genre – enumeration of type Genre, with possible values(Action, Drama, Comedy, Crime, Western, Romance, Documentary, Children, Animation, Musical) (required)
-        //© Software University Foundation.This work is licensed under the CC-BY-NC-SA license.
-        //Follow us: Page 2 of 10
+
+        [Required]
+        public TimeSpan Duration { get; set; }
         //• Duration – TimeSpan (required)
+
+        [Required]
+        [Range(1, 10)]
+        public double Rating { get; set; }
         //• Rating – double in the range [1,10] (required)
+
+        [Required]
+        [MinLength(3), MaxLength(20)]
+        public string Director { get; set; }
         //• Director – text with length[3, 20] (required)
+
+        public ICollection<Projection> Projections { get; set; }
         //• Projections - collection of type Projection
     }
 }

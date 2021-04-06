2eed0e085989e7a8f8922e6dd8020cb5dd9a3ad4
VelinovAngl <kakarum1992@gmail.com>
2021-04-06 23:48:53 +0200

    add DbContext

---
+++ BEST PRACTICES AND ARCHITECTURE/RealEstates/RealEstates.Data/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace RealEstates.Data
+{
+    public static class Configuration
+    {
+        public static string ConnectionString = @"Server=.\SQLEXPRESS;Database=RealEstates;Integrated Security=TRUE;";
+    }
+}

0022f6494212be80913b09584544d34048b3adcc
VelinovAngl <kakarum1992@gmail.com>
2021-04-06 23:52:26 +0200

    add services

---
+++ BEST PRACTICES AND ARCHITECTURE/RealEstates/RealEstates.Services/IPropertiesService.cs
@@ -0,0 +1,7 @@
+﻿namespace RealEstates.Services
+{
+    public interface IPropertiesService
+    {
+        void Add(string district, int floor, int maxFloor, int size, int yardSize, int year, string propertyType, string buildingType, int price);
+    }
+}

46a6907cfcf60b36439142328aee07781749dc9d
VelinovAngl <kakarum1992@gmail.com>
2021-04-18 22:00:32 +0200

    add QuizService and inteface

--- WorkShop/Quiz/Quiz.Model/Quiz.cs
+++ WorkShop/Quiz/Quiz.Model/Quiz.cs
@@ -7,11 +7,14 @@ namespace Quiz.Models
         public Quiz()
         {
             this.Qestions = new HashSet<Question>();
+            this.UserAnswers = new HashSet<UserAnswer>();
         }
         public int Id { get; set; }
 
         public string Titile { get; set; }
 
         public ICollection<Question> Qestions { get; set; }
+
+        public ICollection<UserAnswer> UserAnswers { get; set; }
     }
 }
---
+++ WorkShop/Quiz/Quiz.Services/QuizService.cs
@@ -0,0 +1,26 @@
+﻿namespace Quiz.Services
+{
+    using Quiz.Data;
+    using Quiz.Models;
+
+    public class QuizService : IQuizService
+    {
+        private readonly ApplicationDbContext applicationDbContext;
+
+        public QuizService(ApplicationDbContext applicationDbContext)
+        {
+            this.applicationDbContext = applicationDbContext;
+        }
+
+        public void Add(string title)
+        {
+            var quiz = new Quiz
+            {
+                Titile = title
+            };
+
+            this.applicationDbContext.Quizzes.Add(quiz);
+            this.applicationDbContext.SaveChanges();
+        }
+    }
+}

a35ab2bbc31b11895dc4d3302989973f2fa5d78d
VelinovAngl <kakarum1992@gmail.com>
2021-04-20 22:20:40 +0200

    Add QuestionInputModel and QuizInputModel

--- WorkShop/Quiz/Quiz.Services/UserAnswerService.cs
+++ WorkShop/Quiz/Quiz.Services/UserAnswerService.cs
@@ -3,6 +3,9 @@
     using Quiz.Data;
     using Quiz.Models;
     using Quiz.Services.Contracts;
+    using Quiz.Services.Models;
+    using System.Collections.Generic;
+
     public class UserAnswerService : IUserAnswerService
     {
         private readonly ApplicationDbContext applicationDbContext;
@@ -22,6 +25,27 @@
             };
 
             this.applicationDbContext.UsersAnswers.Add(userAnswer);
+            this.applicationDbContext.SaveChanges();
+        }
+
+        public void BulkAddUserAnswer(QuizInputModel quizInputModel)
+        {
+            var userAnswers = new List<UserAnswer>();
+
+            foreach (var question in quizInputModel.Questions)
+            {
+                var userAnswer = new UserAnswer
+                {
+                    IdentityUserId = quizInputModel.UserId,
+                    QuizId = quizInputModel.QuizId,
+                    AnswerId = question.AnswerId,
+                    QuestionId = question.QuestionId
+                };
+
+                userAnswers.Add(userAnswer);
+            }
+
+            this.applicationDbContext.AddRange(userAnswers);
             this.applicationDbContext.SaveChanges();
         }
     }

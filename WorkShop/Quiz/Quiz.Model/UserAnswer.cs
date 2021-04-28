9cdc7689fdbc7ea7e61aa9cbc60d4eefb08a56e5
VelinovAngl <kakarum1992@gmail.com>
2021-04-28 17:24:39 +0300

    Delete lines

--- WorkShop/Quiz/Quiz.Model/UserAnswer.cs
+++ WorkShop/Quiz/Quiz.Model/UserAnswer.cs
@@ -12,7 +12,6 @@
 
         public int QuestionId { get; set; }
         public Question Question { get; set; }
-
         public int AnswerId { get; set; }
         public Answer Answer { get; set; }
     }

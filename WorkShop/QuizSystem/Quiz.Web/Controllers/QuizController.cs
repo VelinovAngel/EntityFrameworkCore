d7c2583601dcfac824b29ff8328ad3418ead1b6c
VelinovAngl <kakarum1992@gmail.com>
2021-05-08 15:03:38 +0300

    Change Namespaces

--- WorkShop/QuizSystem/Quiz.Web/Controllers/QuizController.cs
+++ WorkShop/QuizSystem/Quiz.Web/Controllers/QuizController.cs
@@ -1,10 +1,6 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Quiz.Services;
 using Microsoft.AspNetCore.Mvc;
-using Quiz.Services;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Quiz.Web.Controllers
 {
